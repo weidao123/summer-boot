22224f8b8be380817662c89b584cb3538f758908
qi.tian <qi.tian@broadfun.cn>
2020-12-30 10:11:45 +0800

    add 文件

--- lib/runtime/summer-cluster.ts
+++ lib/runtime/summer-cluster.ts
@@ -20,9 +20,10 @@ export default class SummerCluster {
     constructor() {
         this.conf = Config.getConfig();
         this.agent = cluster.fork({ NODE_WORK_TYPE: Worker.AGENT });
+        Logger.info(`Master start ${process.pid}`);
 
-        WorkerMessage.once(this.agent, WorkerStatus.AGENT_START_SUCCESS, this.onAgentStart);
-        WorkerMessage.once(this.agent, WorkerStatus.START_FAIL, this.onAgentStartFail);
+        WorkerMessage.once(this.agent, WorkerStatus.AGENT_START_SUCCESS, this.forkWorker.bind(this));
+        WorkerMessage.once(this.agent, WorkerStatus.START_FAIL, this.onAgentStartFail.bind(this));
     }
 
     private onAgentStartFail(data: WorkerMessagePayload) {
@@ -30,23 +31,21 @@ export default class SummerCluster {
         process.exit(1);
     }
 
-    private onAgentStart(data: WorkerMessagePayload) {
+    private forkWorker(data: WorkerMessagePayload) {
         Logger.info(`Agent start pid ${this.agent.process.pid}`);
-
         for (let i = 0; i < this.conf.worker; i++) {
             const worker = cluster.fork();
             this.workers.push(worker);
-            WorkerMessage.once(worker, WorkerStatus.START_SUCCESS, this.onWorkerStart);
+            WorkerMessage.once(worker, WorkerStatus.START_SUCCESS, this.onWorkerStart.bind(this));
         }
         cluster.on("exit", this.onWorkerExit);
     }
 
     private onWorkerStart(data: WorkerMessagePayload<number>) {
         this.count++;
+        Logger.info(`Worker start ${data.data}`);
         if (this.count === this.conf.worker && !this.isStart) {
             this.isStart = true;
-            Logger.info(`Workers: [${this.workers.map(w => w.process.pid)}]`);
-            Logger.info(`Master: [${process.pid}]`);
             Logger.info("Application running hare http://127.0.0.1:" + this.conf.port);
         }
     }
@@ -58,7 +57,7 @@ export default class SummerCluster {
             const w = cluster.fork({ NODE_WORK_TYPE: Worker.WORKER });
             this.count--;
             this.workers.push(w);
-            WorkerMessage.on(w, WorkerStatus.START_SUCCESS, this.onWorkerStart);
+            WorkerMessage.on(w, WorkerStatus.START_SUCCESS, this.onWorkerStart.bind(this));
 
             Logger.warning(`Worker pid=${worker.process.pid} exit, code ${code}, signal ${signal}`);
             Logger.info("fork worker pid " + w.process.pid);
